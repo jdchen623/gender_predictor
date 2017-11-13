d06b71a9acc23256b86eb6d219ec712623c19345
Patrick Mogan <pjmogan@stanford.edu>
2017-11-12 17:34:29 -0800

    trying again

--- test.py
+++ test.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
 from featureExtractor import *
 
-features = extractWordFeatures('data/mary-shelley.txt')
+file = 'data/mary-shelley.txt'
+featureVector = extractWordFeatures(file)
+print("Book: %s" % file)
+print("Exclamation points per sentence: %s" % featureVector['!'])
+print("Commas per sentence: %s" % featureVector[','])
+print("Instances of 'the' per word: %s" % featureVector['the'])
+print("Mean sentence length: %s" % featureVector['sentenceLength'])
